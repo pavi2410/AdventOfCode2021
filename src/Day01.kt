bb8cd7e4ca315ca89cd51d7be0ddfdddd634ec5a
Pavitra Golchha <hello@pavi2410.me>
2021-12-02 00:14:53 +0530

    Day 1: Complete 🥳

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        val incs = depths.zipWithNext()
+        return incs.count { (a, b) -> b > a }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val depths = input.map { it.toInt() }
+        val incs = depths.windowed(3).zipWithNext()
+        return incs.count { (a, b) -> b.sum() > a.sum() }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input)) // 1624
+    println(part2(input)) // 1653
 }
